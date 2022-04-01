dab90438f7f183effd631adfe136777ba24e7522
Nicholas M Martin <nmmart04@louisville.edu>
2022-04-01 18:07:51 -0400

    Add state to rcon page

--- CoralClient/CoralClient/ViewModel/RconPageViewModel.cs
+++ CoralClient/CoralClient/ViewModel/RconPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using CoralClient.Model;
@@ -14,23 +15,41 @@ namespace CoralClient.ViewModel
     {
         private readonly ServerProfile _serverProfile;
 
-        public enum ConnectionStatus
+        public enum State
         {
             DISCONNECTED,
             CONNECTING,
             CONNECTED
         }
 
-        private ConnectionStatus _connectionState;
-        private string _serverUriText = "Server URI";
-        private string _connectionStatusText = "Disconnected";
-        private string _onlinePlayerText = "Players: 0/20";
-        private string _toggleConnectionText = "Connect";
+        private string _serverNameText = "Server URI";
+        private string _connectionStatusText = State.DISCONNECTED.ToString();
+        private string _onlinePlayerText = "Players: ?/?";
+        private string _toggleConnectionButtonText = "Connect";
+        private string _commandLogText;
+        private string _commandEntryText;
+        private readonly StringBuilder _commandLogBuffer = new StringBuilder();
+        private State _currentState;
 
-        public string ServerUriText
+        public event EventHandler StateChange;
+
+        public State LastState { get; set; }
+
+        public State CurrentState
         {
-            get => _serverUriText;
-            set => SetProperty(ref _serverUriText, value);
+            get => _currentState;
+            set
+            {
+                LastState = CurrentState;
+                _currentState = value;
+                StateChange?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public string ServerNameText
+        {
+            get => _serverNameText;
+            set => SetProperty(ref _serverNameText, value);
         }
 
         public string ConnectionStatusText
@@ -45,12 +64,24 @@ namespace CoralClient.ViewModel
             set => SetProperty(ref _onlinePlayerText, value);
         }
 
-        public string ToggleConnectionText
+        public string CommandLogText
         {
-            get => _toggleConnectionText;
-            set => SetProperty(ref _toggleConnectionText, value);
+            get => _commandLogText;
+            set => SetProperty(ref _commandLogText, value);
         }
 
+        public string CommandEntryText
+        {
+            get => _commandEntryText;
+            set => SetProperty(ref _commandEntryText, value);
+        }
+
+        public string ToggleConnectionButtonText
+        {
+            get => _toggleConnectionButtonText;
+            set => SetProperty(ref _toggleConnectionButtonText, value);
+        }
+        
         public ICommand ToggleConnectionCommand { get; }
 
         public ICommand RecentCommandsCommand { get; }
@@ -59,40 +90,96 @@ namespace CoralClient.ViewModel
         {
             _serverProfile = serverProfile;
 
-            ServerUriText = serverProfile.ServerUriText;
+            ServerNameText = serverProfile.ServerUriText;
 
             ToggleConnectionCommand = new Command(
                 execute: async () =>
                 {
-                    switch (_connectionState)
+                    switch (CurrentState)
                     {
-                        case ConnectionStatus.CONNECTED:
-                            _connectionState = await DisconnectAsync();
+                        case State.CONNECTED:
+                            CurrentState = await DisconnectAsync();
                             break;
-                        case ConnectionStatus.DISCONNECTED:
-                            _connectionState = await ConnectAsync();
+                        case State.DISCONNECTED:
+                            CurrentState = await ConnectAsync();
                             break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
                     }
                 },
                 canExecute: () =>
-                    _connectionState != ConnectionStatus.CONNECTING);
+                    CurrentState != State.CONNECTING);
+
+            StateChange += OnStateChanged;
+            StateChange += (sender, args) => ConnectionStatusText = CurrentState.ToString();
         }
 
-        private async Task<ConnectionStatus> ConnectAsync()
+        private void OnStateChanged(object sender, EventArgs e)
         {
-            _connectionState = ConnectionStatus.CONNECTING;
-
-            var host = await Dns.GetHostEntryAsync(_serverProfile.Uri);
-            var result = await ServerQuery.Info(host.AddressList.First(), _serverProfile.MinecraftPort, ServerQuery.ServerType.Minecraft) as MinecraftQueryInfo;
-
-            return ConnectionStatus.CONNECTED;
+            switch (CurrentState)
+            {
+                case State.DISCONNECTED:
+                    {
+                        OnlinePlayerText = "Players: ?/?";
+                        WriteToCommandLog("Disconnected...");
+                        ToggleConnectionButtonText = "Connect";
+                    }
+                    break;
+                case State.CONNECTING:
+                    {
+                        WriteToCommandLog("Connecting...");
+                    }
+                    break;
+                case State.CONNECTED:
+                    {
+                        WriteToCommandLog("Connected!");
+                        ToggleConnectionButtonText = "Disconnect";
+                    }
+                    break;
+            }
         }
 
-        private async Task<ConnectionStatus> DisconnectAsync()
+        private void WriteToCommandLog(string text, bool newLine = true)
         {
-            return ConnectionStatus.DISCONNECTED;
+            var formattedText = $"[{DateTime.Now}] {text}";
+
+            if (newLine)
+            {
+                _commandLogBuffer.AppendLine(formattedText);
+            }
+            else
+            {
+                _commandLogBuffer.Append(formattedText);
+            }
+
+            CommandLogText = _commandLogBuffer.ToString();
+        }
+
+        private async Task<State> ConnectAsync()
+        {
+            CurrentState = State.CONNECTING;
+
+            try
+            {
+                var host = await Dns.GetHostEntryAsync(_serverProfile.Uri);
+                var targetAddress = host.AddressList.First();
+                WriteToCommandLog($"Querying {targetAddress}...");
+                var result = await ServerQuery.Info(targetAddress,
+                    _serverProfile.MinecraftPort,
+                    ServerQuery.ServerType.Minecraft) as MinecraftQueryInfo;
+                WriteToCommandLog($"Connected to Minecraft ({result.Version})");
+                OnlinePlayerText = $"Players: {result.NumPlayers}/{result.MaxPlayers}";
+            }
+            catch (Exception e)
+            {
+                WriteToCommandLog(e.Message);
+                return State.DISCONNECTED;
+            }
+
+            return State.CONNECTED;
+        }
+
+        private async Task<State> DisconnectAsync()
+        {
+            return State.DISCONNECTED;
         }
     }
 }
