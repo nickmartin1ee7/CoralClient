7e70bc96583a333869550a472a956ded7939f601
Nicholas M Martin <nmmart04@louisville.edu>
2022-04-01 19:24:43 -0400

    Add themes

---
+++ CoralClient/CoralClient/View/Themes/DarkTheme.xaml.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoralClient.Themes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DarkTheme : ResourceDictionary
+    {
+    }
+}
