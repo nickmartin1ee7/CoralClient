0f9f66c48ff2ae6c6d0ad77eb70a5f12b7a1b332
Nicholas M Martin <nmmart04@louisville.edu>
2022-04-08 14:34:31 -0400

    Fix xaml formatting. Add icon to main.

--- CoralClient/CoralClient/View/MainPage.xaml.cs
+++ CoralClient/CoralClient/View/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using CoralClient.DbContext;
+﻿/*
+ * Image credit: Coral icons created by Freepik - Flaticon
+ */
+
+using CoralClient.DbContext;
 using CoralClient.Services;
 using CoralClient.ViewModel;
 using Xamarin.Forms;
@@ -16,6 +20,8 @@ namespace CoralClient.View
             var vm = new MainPageViewModel(Dependencies.ServiceProvider.GetService<ServerProfileContext>(),
                 (title, message) => DisplayPromptAsync(title, message),
                 (serverProfile) => Navigation.PushModalAsync(new RconPage(serverProfile)));
+
+            LogoImage.Source = ImageSource.FromResource("CoralClient.Assets.icon.png", GetType().Assembly);
 
             BindingContext = vm;
         }
