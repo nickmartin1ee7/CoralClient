835fde6479402566af0122d01e222fd4bd54b923
Nick Martin <nickmartin1ee7@gmail.com>
2025-09-14 14:04:32 -0400

    remove some debug profiles

--- CoralClientMobileApp/DbContext/ServerProfileContext.cs
+++ CoralClientMobileApp/DbContext/ServerProfileContext.cs
@@ -35,10 +35,6 @@ namespace CoralClientMobileApp.DbContext
                     [
                         new ServerProfile { Uri = "192.168.2.200", MinecraftPort = 25565, RconPort = 25575, Password = "yourStrongestPassword" },
                         new ServerProfile { Uri = "sample1.example.com", MinecraftPort = 25565, RconPort = 25575, Password = "pass1" },
-                        new ServerProfile { Uri = "sample2.example.com", MinecraftPort = 25566, RconPort = 25576, Password = "pass2" },
-                        new ServerProfile { Uri = "sample3.example.com", MinecraftPort = 25567, RconPort = 25577, Password = "pass3" },
-                        new ServerProfile { Uri = "sample4.example.com", MinecraftPort = 25568, RconPort = 25578, Password = "pass4" },
-                        new ServerProfile { Uri = "sample5.example.com", MinecraftPort = 25569, RconPort = 25579, Password = "pass5" }
                     ]);
 
                     await SaveChangesAsync();
